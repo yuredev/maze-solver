9dbc49b25f2f060019e7b0d1e1f4dc7bd6f58828
Yure Matias <yurematias@ufrn.edu.br>
2020-09-19 22:01:30 -0300

    unusued argument maze of move methods removed

--- src/models/position.ts
+++ src/models/position.ts
@@ -16,32 +16,32 @@ export default class Position {
   public isEqual(position: Position) {
     return this.i === position.getI() && this.j === position.getJ();
   }
-  private movePosition(maze: Maze, sumI: number, sumJ: number): Position {
+  private movePosition(sumI: number, sumJ: number): Position {
     return new Position(this.i + sumI, this.j + sumJ);
   }
   private newPositionIsValid(maze: Maze, sumI: number, sumJ: number) {
     return maze.getArray()[this.getI() + sumI][this.getJ() + sumJ] === 0;
   }
   public moveDown(maze: Maze): Position {
-    return this.movePosition(maze, 1, 0);
+    return this.movePosition(1, 0);
   }
   public isFreeBelow(maze: Maze): boolean {
     return this.newPositionIsValid(maze, 1, 0);
   }
   public moveRight(maze: Maze): Position {
-    return this.movePosition(maze, 0, 1);
+    return this.movePosition(0, 1);
   }
   public isFreeInTheRight(maze: Maze): boolean {
     return this.newPositionIsValid(maze, 0, 1);
   }
   public moveUp(maze: Maze): Position {
-    return this.movePosition(maze, -1, 0);
+    return this.movePosition(-1, 0);
   }
   public isFreeAbove(maze: Maze): boolean {
     return this.newPositionIsValid(maze, -1, 0);
   }
   public moveLeft(maze: Maze): Position {
-    return this.movePosition(maze, 0, -1);
+    return this.movePosition(0, -1);
   }
   public isFreeInTheLeft(maze: Maze): boolean {
     return this.newPositionIsValid(maze, 0, -1);
